d013d39e78e5ebfdb4e1a2654c00adef52344e73
Onyeriri Victor <viktorchinonso@gmail.com>
2023-06-10 23:40:27 +0100

    Completed navigation and footer section

--- src/data/data.js
+++ src/data/data.js
@@ -18,14 +18,22 @@ export const socialLinks = [
 ];
 
 export const activities = [
-    { id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.' },
-    { id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.' },
-    { id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.' },
+    { id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.' },
+    { id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.' },
+    { id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.' },
 ];
 
 export const availableTour = [
-    {id: 1, image: tourOne, date: 'august 26th, 2020', title: 'Tibet Adventure', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'china', duration: '6 days', cost: 'from $2100'},
-    {id: 2, image: tourTwo, date: 'october 1th, 2020', title: 'best of java', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'indonesia', duration: '6 days', cost: 'from $2100'},
-    {id: 3, image: tourThree, date: 'september 15th, 2020', title: 'explore hong kong', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'hong kong', duration: '8 days', cost: 'from $5000'},
-    {id: 4, image: tourFour, date: 'december 5th, 2019', title: 'kenya highlights', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'kenya', duration: '20 days', cost: 'from $3300'},
-    ]
+    { id: 1, image: tourOne, date: 'august 26th, 2020', title: 'Tibet Adventure', name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'china', duration: '6 days', cost: 'from $2100' },
+    { id: 2, image: tourTwo, date: 'october 1th, 2020', title: 'best of java', name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'indonesia', duration: '6 days', cost: 'from $2100' },
+    { id: 3, image: tourThree, date: 'september 15th, 2020', title: 'explore hong kong', name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'hong kong', duration: '8 days', cost: 'from $5000' },
+    { id: 4, image: tourFour, date: 'december 5th, 2019', title: 'kenya highlights', name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', location: 'kenya', duration: '20 days', cost: 'from $3300' },
+];
+
+export const footerLinks = [
+    { id: 1, href: '#home', className: 'footer-link', name: 'home' },
+    { id: 2, href: '#about', className: 'footer-link', name: 'about' },
+    { id: 3, href: '#services', className: 'footer-link', name: 'services' },
+    { id: 4, href: '#featured', className: 'footer-link', name: 'featured' },
+
+];
